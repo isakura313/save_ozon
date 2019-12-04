a9bcb00540bc1e225b370c5a7e1add7ba948d26e
Paul <isakura313@gmail.com>
2019-12-04 16:43:31 +0300

    new

--- median.py
+++ median.py
@@ -1,19 +1,22 @@
-print("Введите количество элементов")
+print("введите название файла без расширения, который вы перенесли в эту папку")
 
-iter = int(input())
+fileName = input("Ваше число: ")
+fileName = fileName + ".txt"
+f = open(fileName, "r");
+file_content = f.read()
+file_arr = file_content.split(',')
 
-list_num = []
+print(file_arr)
+list_num = file_arr
 
 # функция которая получает пишет значения в массив
-def get_data(count):
-	for i in range(count):
-		x = int(input())
-		list_num.append(x)
-get_data(iter)
+for i in range(len(list_num)):
+	list_num[i] = int(list_num[i])
+
 
 def average(arr):
 	sum = 0
-	for i in range arr:
+	for i in range(len(list_num)):
 		sum += arr[i]
 	return sum/len(arr)
 print(average(list_num))
